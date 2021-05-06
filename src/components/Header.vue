298ada66ee81f4855052b2be4570013029deab42
mae <myeongheeroh@gmail.com>
2021-05-06 17:12:55 +0900

    Add Button.vue @click methods

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,21 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
+        <Button text="Add Task" color="navy"/>
     </header>
 </template>
 
 <script>
+import Button from './Button'
+
 export default {
     name: 'Header',
     props: {
-        title: {
-            type: String,
-            default: 'Hello World',
-        }
+        title: String,
     },
+    components: {
+        Button,
+    }
 }
 </script>
 
